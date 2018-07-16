5ee267c8a8f2809d6b39ab25b5fc6d68064cee26
QNH-NET\martijn.doornik <martijn.doornik@qnh.nl>
2018-07-16 13:09:25 +0200

    In good conditions, the status monitor works. Requires more fixes if taken into production.

--- services/status-monitor/api/server.js
+++ services/status-monitor/api/server.js
@@ -6,11 +6,29 @@ const WebSocketServer = require('websocket').server;
 
 const history = {};
 
+const onMessage = (message) => {
+    if (message.type === 'utf8') {
+        const json = JSON.parse(message.utf8Data);
+        const request = json.request;
+        if (request === 'testError') {
+            eventHub.error('This is merely a test');
+        } else if (request === 'testOffline') {
+            const body = {};
+            body[eventDataList.STATUS_CODE] = eventDataList.STATUS_CODES.OFFLINE;
+            body[eventDataList.ERROR_MESSAGE] = ['This is a test', 'Offline response requested by status-monitor-api.onMessage'];
+            eventHub.send(eventList.STATUS_RESPONSE, body);
+        } else if (request === 'refresh') {
+            eventHub.send(eventList.STATUS_REQUEST);
+        }
+    }
+}
+
 const onRequest = (client) => {
     const connection = client.accept(null, client.origin);
+    connection.on('message', onMessage);
     const message = {
-        "type": "history",
-        "history": history
+        type: "history",
+        history: history
     }
     connection.sendUTF(JSON.stringify(message));
 }
@@ -21,8 +39,8 @@ const onDisconnect = (client) => {
 
 const api = ((config) => {
     const http = require('http');
-    const httpServer = http.createServer((request, response) => {});
-    httpServer.listen(config['server']['port'], () => {});
+    const httpServer = http.createServer((request, response) => { });
+    httpServer.listen(config['server']['port'], () => { });
     const self = new WebSocketServer({
         httpServer: httpServer
     });
@@ -32,27 +50,50 @@ const api = ((config) => {
 })(config);
 
 const onEvent = (eventName, eventData, eventTime) => {
+    const sender = eventData[eventDataList.SENDER];
+    if (!history[sender]) history[sender] = {name: sender};
     if (eventName === eventList.STATUS_RESPONSE) {
-        const sender = eventData[eventDataList.SENDER];
         const status = eventData[eventDataList.STATUS_CODE];
         const statusMessage = eventData[eventDataList.ERROR_MESSAGE];
-        const message = {
-            status: status,
-            message: statusMessage,
-            time: eventTime
-        };
-        history[sender] = message;
-        message['name'] = sender;
-        message['type'] = "update";
-        api.broadcast(JSON.stringify(message));
+        console.log('Received status of "' + sender + '" on ' + (new Date(eventTime)).toISOString().split('T')[0] + ': ' + status);
+        history[sender].status = status;
+        history[sender].statusMessage = statusMessage;
+        history[sender].time = eventTime;
+        sendUpdate(sender);
+    } else if (eventName === eventList.VERSION_RESPONSE) {
+        const version = eventData[eventDataList.VERSION_RELEASE] + '.' +
+            eventData[eventDataList.VERSION_PATCH] + '.' +
+            eventData[eventDataList.VERSION_HOTFIX];
+        history[sender].version = version;
+        sendUpdate(sender);
+    }
+}
+
+const sendUpdate = (name) => {
+    if (!!history[name]) {
+        try {
+            // clone history object
+            const message = JSON.parse(JSON.stringify(history[name]));
+            message['name'] = name;
+            message['type'] = "update";
+            api.broadcast(JSON.stringify(message));
+        } catch (error) {
+            console.log(history[name]);
+        }
     }
 }
 
 eventHub.configure(config, require('./package.json'));
 eventHub.start(onEvent);
-const interval = setInterval(() => {
+console.log('Listening on eventhub');
+console.log('Requesting current listeners...');
+setTimeout(() => {
+    eventHub.send(eventList.VERSION_REQUEST);
+    eventHub.send(eventList.STATUS_REQUEST);
+}, 1000);
+/*const interval = setInterval(() => {
     console.log('Pinging new versions...');
     eventHub.send(eventList.STATUS_REQUEST);
-}, config['server']['interval']);
+}, config['server']['interval']);*/
 
 
